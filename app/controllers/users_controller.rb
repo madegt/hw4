2d25da97ff2114feb047bbb18dcb423bcde2bf02
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-03-08 17:21:19 +0000

    new_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
+    @user["username"] = params["username"]
+      @user["email"] = params["email"]
     @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
     flash[:notice] = "Thanks for signing up. Now login."
